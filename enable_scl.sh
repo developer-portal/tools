c90298c5d102980f38d903a6cabe3bb71e971f80
Pavel Valena <pvalena@redhat.com>
2017-03-15 16:58:29 +0100

    Simplify enable_scl.sh

--- enable_scl.sh
+++ enable_scl.sh
@@ -1,9 +1,6 @@
 
 [[ "$SCL_RUBY" ]] && {
-  bash -c '. /etc/os-release ; grep -q "Red Hat Enterprise Linux" <<< "$NAME" ; exit $?' && {
-     . scl_source enable "$SCL_RUBY" || die "$F scl_source enable $SCL_RUBY"
-    scl_enabled "$SCL_RUBY" || die "$F enable $SCL_RUBY"
-
-  }
+   . scl_source enable "$SCL_RUBY" || die "$F scl_source enable $SCL_RUBY"
+  scl_enabled "$SCL_RUBY" || die "$F enable $SCL_RUBY"
 
 }
