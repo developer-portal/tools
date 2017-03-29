9c66c76d79d57b7c4830b4e62914182ae9232aa6
Pavel Valena <pvalena@redhat.com>
2017-03-29 11:05:35 +0200

    Pass all arguments to devel.sh
    from run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #
-# ./run.sh
+# ./run.sh [anything]
 #   Run devel.sh in continuous mode and restart it on failure.
 #   Intended for service-like usage.
 #   See './common.sh -h' and others for more info.
 #
+#   All args are passed to devel.sh
 #
 
- [[ "$1" ]] && { echo 'No help here! `head` this file ;)' ; exit 0 ; }
+ [[ "$1" == "-h" ]] && { echo 'No help here! `head` this file ;)' ; exit 0 ; }
 
  D="$(readlink -f "`dirname "$0"`")"
  F="`readlink -f "$D/devel.sh"`"
@@ -15,8 +16,8 @@
  [[ -x "$F" ]] || { echo "Could not exec '$F' in '$D'" ; exit 1 ; }
 
 while :; do
-  echo -e "\n--> Running $F -c"
-  $F -c
+  echo -e "\n--> Running $F -c $@"
+  $F -c "$@"
   echo '=== FAIL ==='
   sleep 300
 
