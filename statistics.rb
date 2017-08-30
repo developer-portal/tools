6491e9f0585c63f12159a2a1bc05480657d07ddd
Pavel Valena <pvalena@redhat.com>
2017-08-30 19:50:23 +0200

    New: statistics.rb - generate table from Google Analytics
    example input: stats.txt

---
+++ statistics.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/ruby
+#   ./statistics.rb
+#     Generates table from Google Analytics 'web table' copy-pasted into a file.
+#
+#   Example:
+#     ./statistics.rb stats.txt
+#
+
+d = Hash.new
+l = nil
+ins = -> (z) {
+  d[l] ||= Array.new
+  d[l].push(z) if d[l].size < 3
+}
+
+d['Statistics for last 30 days'] = %w()
+
+l = '<insert_____________date>'
+d[l] = %w(Pageviews Unique\ Pageviews Avg.\ Time\ on\ Page)
+
+k = l
+l = l.gsub(/./,'=')
+d[k].each { |a|
+  ins.call(a.gsub(/./,'='))
+}
+
+l = 'Total'
+ARGF.each { |b| 
+  next if b[0] == '%'
+  b.squeeze(' ').split(' ').each { |a|
+    if a[0] == '/' ; l=a else ins.call(a) end
+  }
+}
+
+o = d.map { |x| x.join('|') }
+puts `column -ts '|' <<< "#{o.join("\n  ")}"`
