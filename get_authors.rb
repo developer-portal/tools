10005944473149743f5c9852bf3ea0a80a6d9c0a
jackorp <jar.prokop@volny.cz>
2018-03-19 16:45:52 +0100

    Add working get_authors.rb script

--- get_authors.rb
+++ get_authors.rb
@@ -1,7 +1,22 @@
 #!/usr/bin/ruby
+# This script gets last 5 authors on a file and writes them on the end.
 mdfiles = File.join("*","**","*.md")
 files = Dir.glob(mdfiles)
-command = 'git log -5 --pretty=format:"%an;%ae" README.md'
-#require 'irb'; binding.irb
-puts %x( #{command} )
-#do |mdfiles|
+def create_md (a,e)
+  "[#{a}](#{e})"
+end
+files.each do |f|
+  author_mail = {}
+  author_md = []
+  command = ( 'git log -5 --pretty=format:"%an;%ae" ' )
+  commandOut = %x( #{command}#{f} ).split(/[;\n]/)
+  author_mail = commandOut.each_slice(2).to_h.sort
+  author_mail.each do |a,e|
+    author = create_md(a,e)
+    author_md << author
+  end
+  output = author_md.join(", ")
+  File.open(f, "a") do |file|
+    file.write("\nAuthors: #{output}")
+  end
+end
