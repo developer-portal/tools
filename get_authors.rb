7b661badc64058089c644da46ee5b63e7e89bfbf
jackorp <jar.prokop@volny.cz>
2018-03-19 22:41:16 +0100

    Fix rubocop offenses

--- get_authors.rb
+++ get_authors.rb
@@ -1,22 +1,22 @@
 #!/usr/bin/ruby
 # This script gets last 5 authors of a file and writes them at the end of the file.
-mdfiles = File.join("*","**","*.md")
+mdfiles = File.join('*', '**', '*.md')
 files = Dir.glob(mdfiles)
-def create_md (a,e)
+def create_md(a, e)
   "[#{a}](#{e})"
 end
 files.each do |f|
   author_mail = {}
   author_md = []
-  command = ( 'git log -5 --pretty=format:"%an;%ae" ' )
-  commandOut = %x( #{command}#{f} ).split(/[;\n]/)
-  author_mail = commandOut.each_slice(2).to_h.sort
-  author_mail.each do |a,e|
-    author = create_md(a,e)
+  command = 'git log -5 --pretty=format:"%an;%ae" '
+  command_out = %x(#{command}#{f}).split(/[;\n]/)
+  author_mail = command_out.each_slice(2).to_h.sort
+  author_mail.each do |a, e|
+    author = create_md(a, e)
     author_md << author
   end
-  output = author_md.join(", ")
-  File.open(f, "a") do |file|
+  output = author_md.join(', ')
+  File.open(f, 'a') do |file|
     file.write("\nAuthors: #{output}")
   end
 end
