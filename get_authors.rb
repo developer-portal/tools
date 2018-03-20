761663b309053fafa32ca187df88aa66a24a6408
Pavel Valena <pvalena@redhat.com>
2018-03-20 23:54:34 +0100

    get_authors: enhance formatting a bit & center authors on page

--- get_authors.rb
+++ get_authors.rb
@@ -1,22 +1,32 @@
 #!/usr/bin/ruby
 # This script gets last 5 authors of a file and writes them at the end of the file.
-mdfiles = File.join('*', '**', '*.md')
-files = Dir.glob(mdfiles)
-def create_md(a, e)
+
+def md_mailto(a, e)
   "[#{a}](#{e})"
 end
+
+def md_center(text)
+  '{:center: style="text-align: center"}' + "\n#{text}\n{:center}"
+end
+
+files = Dir.glob File.join('*', '**', '*.md')
+
 files.each do |f|
   author_mail = {}
   author_md = []
   command = 'git log -5 --pretty=format:"%an;%ae" '
   command_out = %x(#{command}#{f}).split(/[;\n]/)
   author_mail = command_out.each_slice(2).to_h.sort
+
   author_mail.each do |a, e|
-    author = create_md(a, e)
-    author_md << author
+    author_md << md_mailto(a, e)
   end
-  output = author_md.join(', ')
+
+  output = author_md.join ', '
+  output = "Authors: #{output}"
+  output = md_center output
+
   File.open(f, 'a') do |file|
-    file.write("\nAuthors: #{output}")
+    file.write "\n#{output}"
   end
 end
