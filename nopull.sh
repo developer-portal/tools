525b0850fa905edd9dcb3123131153b697ff77b6
Pavel Valena <pvalena@redhat.com>
2018-04-09 20:13:48 +0200

    nopull.sh: just build and the website and content
    without checking out or resetting git

---
+++ nopull.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# ./nopull.sh BRANCH [common]
+#   Simple build, no rsync to server or git push.
+#
+# BRANCH    branch for commit on git
+#
+
+  . $(dirname "`readlink -e "$0"`")/common.sh
+[[ "$SITE" ]] || exit 1
+[[ -n "$1" ]] || die 'you have to specify a branch.'
+
+[[ "$1" == "devel" ]] && {
+  scd "website"
+  echo > _includes/announcement.html
+}
+
+ buildsite
+
+ MSG="${1^} update\n"
+
+ prepgit "$1"
