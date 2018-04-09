c25e2c014fe18bf6861e7b930d4bddeb75860ec6
Pavel Valena <pvalena@redhat.com>
2018-04-09 20:15:25 +0200

    email.sh: generates links for new-fdp-release email from git

---
+++ email.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# ./email.sh [common] [-s]
+#   Get links for email.
+#   Does not do any changes.
+#   For common options, see './common.sh -h'.
+#
+#   -s    output for staging server
+#
+
+ . $(dirname "`readlink -e "$0"`")/common.sh &>/dev/null
+
+U1='https://developer.'
+U2='fedoraproject.org/'
+
+stg=
+ind=
+
+[[ "$1" == '-s' ]] && { stg='stg.' ; ind='  ' ; shift ; }
+URL="$U1$stg$U2"
+
+[[ -d "$SITE" ]] || exit 1
+cd "$SITE" || exit 1
+
+c=0
+
+git log --numstat master -1 \
+  | grep '^[0-9]' \
+  | tr -s '\t' ' ' \
+  | cut -d' ' -f3 \
+  | grep -vE '^(js/index.json|sitemap.xml|deployment.html|start.html|tech.html|tools.html)$' \
+  | while read x; do
+    let 'c+=1'
+    echo "${ind}[$c] ${URL}$x"
+  done
