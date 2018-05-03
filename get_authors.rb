528f8e2b9f523b958a605cabbfa141499e171c60
jackorp <jar.prokop@volny.cz>
2018-05-03 20:10:07 +0200

    Add Markdown module and tests
    Write method "main" so code wouldn't execute upon require.

--- get_authors.rb
+++ get_authors.rb
@@ -6,26 +6,34 @@ def get_authors(file)
   `git log -5 --pretty=format:"%an;%ae" #{file}`.split(/[;\n]/)
 end
 
-def md_mailto(a, e)
-  "[#{a}](mailto:#{e})"
-end
-
-def md_center(text)
-  '{:center: style="text-align: center"}' + "\n#{text}\n{:center}"
-end
-
-files = Dir.glob File.join('*', '**', '*.md')
-
-files.each do |f|
-  author_mail = get_authors(f).each_slice(2).to_h.sort
-
-  author_md = author_mail.collect { |a, e| md_mailto(a, e) }
-
-  output = author_md.join ', '
-  output = "Authors: #{output}"
-  output = md_center output
-
-  File.open(f, 'a') do |file|
-    file.write "\n#{output}"
+module Markdown
+  def self.mailto(a, e)
+    "[#{a}](mailto:#{e})"
   end
+
+  def self.center(text)
+    '{:center: style="text-align: center"}' + "\n#{text}\n{:center}"
+  end
+end
+
+def main
+  files = Dir.glob File.join('*', '**', '*.md')
+
+  files.each do |f|
+    author_mail = get_authors(f).each_slice(2).to_h.sort
+
+    author_md = author_mail.collect { |a, e| Markdown.mailto(a, e) }
+
+    output = author_md.join ', '
+    output = "Authors: #{output}"
+    output = Markdown.center output
+
+    File.open(f, 'a') do |file|
+      file.write "\n#{output}"
+    end
+  end
+end
+
+if File.identical?(__FILE__, $0)
+  main
 end
