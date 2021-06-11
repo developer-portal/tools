06d3020caf5faa5d5a13844265b65167b1a46d3f
Pavel Valena <pvalena@redhat.com>
2021-06-11 10:56:39 +0200

    Fixup and enhance container.sh.

--- container.sh
+++ container.sh
@@ -1,31 +1,44 @@
 #!/bin/bash
 #
-# ./commit.sh [common]
+# ./container.sh [common][options]
 #   Prepare staging branch for release.
 #   Does only local changes.
-#   For common options, see './common.sh -h'.
+#
+# Options:
+#   -u    Update to latest website+content git commits
+#
+# For common options, see './common.sh -h'.
 #
 
  . $(dirname "`readlink -e "$0"`")/common.sh || exit 1
 
-  MSG='Content & Website update\n'
+[[ "$1" == "-u" ]] && {
+  shift
+  upgit "website"
+  upgit "website/content"
+}
 
-  scd "website"
+MSG='Content & Website update\n'
 
-  vrun 'echo > _includes/announcement.html'
+scd "website"
 
-  # container run
-  vrun 'docker run -d --rm -p4000:4000 -v $PWD:/opt/developerportal/website:Z quay.io/developer-portal/start'
+vrun 'echo > _includes/announcement.html'
 
-  sleep 10
+# container run
+vrun 'podman run --pull=always -d -p4000:4000 -v $PWD:/opt/developerportal/website:Z quay.io/developer-portal/devel'
 
-  # container copy
-  vrun 'podman container cp "`podman ps -lq`:/opt/developerportal/website/_site/" .'
+sleep 10
 
-  vrun 'podman stop -l'
+vrun 'podman logs -l'
+vrun 'podman ps -l --format "{{.Status}}" | grep "^Up "'
 
-  cleanup
+# container copy
+vrun 'podman container cp "`podman ps -lq`:/opt/developerportal/website/_site/" .'
 
-  prepgit master
+vrun 'podman stop -l'
 
-  logg "Done\n"
+cleanup
+
+prepgit master
+
+logg "Done\n"
